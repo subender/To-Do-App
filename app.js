1f38b7e18b40dce0bd37efb2070e42a799aba33d
subender <subender.io@outlook.com>
2022-03-04 02:41:25 +0530

    completed HTML and CSS part

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("working");
+const inputField = document.querySelector(".input__field");
+const button = document.querySelector(".btn");
+const todoList = document.querySelector(".todo__list");
+
+button.addEventListener("click", addTask);
+
+function addTask() {}
