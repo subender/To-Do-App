c4dbd32f11bdef655283fa864454bb7d4480ae62
subender <subender.io@outlook.com>
2022-03-05 16:32:39 +0530

    Implemented local storage functionality and some function refactoring

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const inputFieldEl = document.querySelector(".input__field");
 const addButton = document.querySelector(".btn");
 const todoListEl = document.querySelector(".todo__list");
 const filterTaskEl = document.querySelector(".filter__tasks");
+let taskArr;
+
+// To get Data from local storage
+const tasks = JSON.parse(localStorage.getItem("tasks"));
+if (tasks) {
+  tasks.forEach((task) => {
+    createTask(task);
+  });
+}
 
 // Event Listners
 
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
-  if (inputFieldEl.value !== "") {
-    createTask(inputFieldEl.value);
-  }
-
-  inputFieldEl.value = "";
+  if (inputFieldEl.value !== "") createTask(inputFieldEl.value);
 });
 
 todoListEl.addEventListener("click", deleteTask);
+
 todoListEl.addEventListener("click", markComplete);
 
 filterTaskEl.addEventListener("click", filterTasks);
@@ -37,6 +43,12 @@ function createTask(value) {
     `;
 
   todoListEl.appendChild(task);
+
+  // Adding task to local storage
+  addToLS(inputFieldEl.value);
+
+  // Clear input field value
+  inputFieldEl.value = "";
 }
 
 //Performing Deletion of a Task
@@ -51,6 +63,7 @@ function deleteTask(e) {
     //Delete Animation
 
     task.classList.add("drop");
+    removeFromLS(task);
 
     //waiting for the delete animation to complete.
     task.addEventListener("transitionend", () => {
@@ -60,10 +73,9 @@ function deleteTask(e) {
   }
 }
 
+//Mark completed task.
 function markComplete(e) {
   const item = e.target;
-
-  console.log(item.classList);
 
   if (item.classList[1] === "done") {
     const task = item.closest(".todo");
@@ -72,6 +84,7 @@ function markComplete(e) {
   }
 }
 
+// Filtering tasks.
 function filterTasks(e) {
   const tasks = todoListEl.childNodes;
   tasks.forEach((task) => {
@@ -94,6 +107,35 @@ function filterTasks(e) {
         } else {
           task.style.display = "none";
         }
+        break;
     }
   });
+}
+
+// Checks for Existing Local Storage Array
+function createLS(task) {
+  if (localStorage.getItem("tasks") === null) {
+    taskArr = [];
+  } else {
+    taskArr = JSON.parse(localStorage.getItem("tasks"));
+  }
+}
+
+//Adds Items To Local Storage
+function addToLS(task) {
+  createLS(task);
+
+  if (task !== "") {
+    taskArr.push(task);
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(taskArr));
+}
+
+//Removes items from Local Storage
+function removeFromLS(task) {
+  createLS(task);
+  const taskIndex = task.children[0].innerText;
+  taskArr.splice(taskArr.indexOf(taskIndex), 1);
+  localStorage.setItem("tasks", JSON.stringify(taskArr));
 }
